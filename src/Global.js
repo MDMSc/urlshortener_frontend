768641331a1cddccb3d8fc51f10345e47faaaf40
MDMSc <m.s.choudhury96@gmail.com>
2022-12-25 21:17:16 +0530

    adding netlify file and changing API

--- src/Global.js
+++ src/Global.js
@@ -1,3 +1,3 @@
-export const API_HOME = "http://localhost:3002";
+export const API_HOME = "https://shrinker-kufc.onrender.com";
 export const API_USERS = `${API_HOME}/api/users`;
 export const API_SHRINKER = `${API_HOME}/api/shrinker`;
